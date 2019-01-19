57aa441a695d7b44497504d47a9601058b5b7329
GitHubAhmad <46747674+GitHubAhmad@users.noreply.github.com>
2019-01-19 13:39:15 +0300

    NewProject

---
+++ Dotween/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraScript : MonoBehaviour {
+    public float duration;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        transform.DOMoveZ(992, duration);
+	}
+}
---
+++ Dotween/Assets/Scripts/Dotween.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Dotween : MonoBehaviour {
+    public bool myBool;
+    public int duration;
+    public GameObject Ahmad;
+
+    public Material MyMat;
+    public Light MyLight;
+    public Vector3[] Points;
+
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        //Points = new Vector3[5];
+       // Points[0] = transform.position;
+        
+	}
+	        
+	// Update is called once per frame
+	void Update () {
+
+        //transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+        // _______________________ DOTWEEN(Transform)__________________________
+
+        //gameObject.transform.DOMove(new Vector3(0,0,0),5, !myBool);
+        //gameObject.transform.DOMoveX(500, duration, myBool);
+        //gameObject.transform.DOMoveY(500, duration, myBool);
+        //gameObject.transform.DOMoveZ(500, duration, myBool);
+        //gameObject.transform.DOLocalMoveX(500, duration, myBool);
+        //gameObject.transform.DOLocalMoveY(500, duration, myBool);
+        //gameObject.transform.DOLocalMoveZ(500, duration, myBool);
+        //gameObject.transform.DOJump(new Vector3(0, 1, 0), 1, 5, 1, !myBool);//dont work
+        //gameObject.transform.DORotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
+        //gameObject.transform.DOLocalRotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
+        //gameObject.transform.DOLookAt(Ahmad.transform.position,duration,AxisConstraint.None,Vector3.up);
+        //gameObject.transform.DOScale(new Vector3(8, 95, 999), duration);
+        //gameObject.transform.DOPunchPosition(transform.position, sduration, 10, 1, myBool);
+        //gameObject.transform.DOPunchScale(transform.position, duration, 10, 1);
+        //gameObject.transform.DOPath(Points, duration, PathType.Linear,PathMode.Full3D,10,Color.red);__!__
+        //gameObject.transform.DOBlendableMoveBy(Ahmad.transform.position, duration, myBool);__!__
+        //gameObject.transform.DOBlendableRotateBy(Ahmad.transform.position, duration, RotateMode.Fast);__!__
+        //gameObject.transform.DOBlendableScaleBy(Ahmad.transform.position, duration);__!__
+        // _______________________ DOTWEEN(Light)__________________________
+        // MyLight.DOColor(Color.blue, duration);
+        // MyLight.DOIntensity(50, duration);
+        //MyLight.DOShadowStrength(1, duration);
+        // _______________________ DOTWEEN(Material)__________________________
+        //MyMat.DOColor(Color.yellow, duration);
+        //MyMat.DOFade(0, duration);__!__
+        // _______________________ DOTWEEN(RigidBody)__________________________
+        gameObject.GetComponent<Rigidbody>().DOMove(Vector3.forward, duration, myBool);
+    }
+
+}
