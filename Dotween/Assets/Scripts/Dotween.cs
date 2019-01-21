c30ad26698cdc2b97fb7225a6b690fc7c3810910
GitHubAhmad <46747674+GitHubAhmad@users.noreply.github.com>
2019-01-21 10:54:09 +0300

    EditTheCode

--- Dotween/Assets/Scripts/Dotween.cs
+++ Dotween/Assets/Scripts/Dotween.cs
@@ -23,27 +23,26 @@ public class Dotween : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        //transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+        transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * Speed * Time.deltaTime);
         //_______________________ DOTWEEN(Transform)__________________________
-
-        //gameObject.transform.DOMove(new Vector3(0, 0, 0), 5, !myBool);
-        //gameObject.transform.DOMoveX(500, duration, myBool);
-        //gameObject.transform.DOMoveY(500, duration, myBool);
-        //gameObject.transform.DOMoveZ(500, duration, myBool);
-        //gameObject.transform.DOLocalMoveX(500, duration, myBool);
-        //gameObject.transform.DOLocalMoveY(500, duration, myBool);
-        //gameObject.transform.DOLocalMoveZ(500, duration, myBool);
-        //gameObject.transform.DOJump(new Vector3(0, 1, 0), 1, 5, 1, !myBool);//dont work
-        //gameObject.transform.DORotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
-        //gameObject.transform.DOLocalRotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
-        //gameObject.transform.DOLookAt(Player.transform.position, duration, AxisConstraint.None, Vector3.up);
-        //gameObject.transform.DOScale(new Vector3(8, 95, 999), duration);
-        //gameObject.transform.DOPunchPosition(transform.position, duration, 10, 1, myBool);
-        //gameObject.transform.DOPunchScale(transform.position, duration, 10, 1);
-        //gameObject.transform.DOPath(Points, duration, PathType.Linear, PathMode.Full3D, 10, Color.red); __!__
-        //gameObject.transform.DOBlendableMoveBy(Player.transform.position, duration, myBool); __!__
-        //gameObject.transform.DOBlendableRotateBy(Player.transform.position, duration, RotateMode.Fast); __!__
-        //gameObject.transform.DOBlendableScaleBy(Player.transform.position, duration); __!__
+        //Player.transform.DOMove(new Vector3(0, 0, 0), 5, !myBool);
+        //Player.transform.DOMoveX(500, duration, myBool);
+        //Player.transform.DOMoveY(500, duration, myBool);
+        //Player.transform.DOMoveZ(500, duration, myBool);
+        //Player.transform.DOLocalMoveX(500, duration, myBool);
+        //Player.transform.DOLocalMoveY(500, duration, myBool);
+        //Player.transform.DOLocalMoveZ(500, duration, myBool);
+        //Player.transform.DOJump(new Vector3(0, 1, 0), 1, 5, 1, !myBool);//dont work
+        //Player.transform.DORotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
+        //Player.transform.DOLocalRotate(new Vector3(50, 90, 80), duration, RotateMode.Fast);
+        //Player.transform.DOLookAt(Player.transform.position, duration, AxisConstraint.None, Vector3.up);
+        //Player.transform.DOScale(new Vector3(8, 95, 999), duration);
+        //Player.transform.DOPunchPosition(transform.position, duration, 10, 1, myBool);
+        //Player.transform.DOPunchScale(transform.position, duration, 10, 1);
+        //Player.transform.DOPath(Points, duration, PathType.Linear, PathMode.Full3D, 10, Color.red); __!__
+        //Player.transform.DOBlendableMoveBy(Player.transform.position, duration, myBool); __!__
+        //Player.transform.DOBlendableRotateBy(Player.transform.position, duration, RotateMode.Fast); __!__
+        //Player.transform.DOBlendableScaleBy(Player.transform.position, duration); __!__
         // _______________________ DOTWEEN(Light)__________________________
         // MyLight.DOColor(Color.blue, duration);
         //MyLight.DOIntensity(50, duration);
