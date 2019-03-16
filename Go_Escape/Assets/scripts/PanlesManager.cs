8ed07be9a8ccde43d829e13f158c038f09c3d820
GitHubAhmad <46747674+GitHubAhmad@users.noreply.github.com>
2019-03-16 09:15:30 +0300

    Complete Main_Menu UI

--- Go_Escape/Assets/scripts/PanlesManager.cs
+++ Go_Escape/Assets/scripts/PanlesManager.cs
@@ -6,12 +6,12 @@ using DG.Tweening;
 public class PanlesManager : MonoBehaviour
 {
     public GameObject Main_menu;
-    public GameObject[] Main_Menu_Objects;
+    public RectTransform[] Main_Menu_Objects;
     public Vector3[] Initial_Transforms_MainMenu;
     public Vector3[] Initial_Scale_MainMenu;
 
     public GameObject Info;
-    public GameObject Info_Object;
+    public RectTransform Info_Object;
     public Vector3 Initial_Transform;
     public float Info_new_Pos;
 
@@ -25,39 +25,42 @@ public class PanlesManager : MonoBehaviour
 
     public Vector3 circle;
 
-
+    public Sprite a;
     public void Main_Menu_Tweens()
     {
         Main_menu.SetActive(true);
         Info.SetActive(false);
         Main_Menu_Objects[0].transform.DOScale(Main_Menu_New_Scals[0], Duration);
         Main_Menu_Objects[1].transform.DOScale(Main_Menu_New_Scals[1], Duration2);
-        Main_Menu_Objects[1].transform.DOMoveX(Main_Menu_New_Pos[4], Duration2).SetEase(Ease.OutExpo);
-        Main_Menu_Objects[1].transform.DORotate(circle, Duration2, RotateMode.FastBeyond360);
         Main_Menu_Objects[2].transform.DOScale(Main_Menu_New_Scals[2], Duration);
-        Main_Menu_Objects[3].transform.DOMoveX(Main_Menu_New_Pos[0], Duration2);
-        Main_Menu_Objects[4].transform.DOMoveY(Main_Menu_New_Pos[1], Duration2 * 1 / 4).SetEase(Ease.OutBack);
-        Main_Menu_Objects[5].transform.DOMoveY(Main_Menu_New_Pos[2], Duration2 * 1 / 3).SetEase(Ease.OutBack);
-        Main_Menu_Objects[6].transform.DOMoveY(Main_Menu_New_Pos[2], Duration2 * 1 / 2).SetEase(Ease.OutBack);
-        Main_Menu_Objects[7].transform.DOMoveY(Main_Menu_New_Pos[3], Duration2 * 1).SetEase(Ease.OutBack);
+
+        Main_Menu_Objects[1].transform.localRotation = Quaternion.Euler(0, 0, 360);
+            //DOLocalRotate(circle, Duration2, RotateMode.FastBeyond360);
+
+        Main_Menu_Objects[3].transform.DOLocalMoveX(Main_Menu_New_Pos[0], Duration2);       
+        Main_Menu_Objects[4].transform.DOLocalMoveY(Main_Menu_New_Pos[1], Duration2 * 1 / 4).SetEase(Ease.OutBack);
+        Main_Menu_Objects[5].transform.DOLocalMoveY(Main_Menu_New_Pos[2], Duration2 * 1 / 3).SetEase(Ease.OutBack);
+        Main_Menu_Objects[6].transform.DOLocalMoveY(Main_Menu_New_Pos[3], Duration2 * 1 / 2).SetEase(Ease.OutBack);
+        Main_Menu_Objects[7].transform.DOLocalMoveY(Main_Menu_New_Pos[4], Duration2 * 1).SetEase(Ease.OutBack);
+        Main_Menu_Objects[1].transform.DOLocalMoveX(Main_Menu_New_Pos[5], Duration2).SetEase(Ease.OutExpo);
     }
    
     public void Info_Tweens()
     {
         Main_menu.SetActive(false);
         Info.SetActive(true);
-        Info_Object.transform.DOMoveX(Info_new_Pos, Duration2);
+        Info_Object.transform.DOLocalMoveX(Info_new_Pos, Duration2);
 
     }
     public void Set_Pos_Main_Menu()
     {
         DOTween.KillAll();
-        Main_Menu_Objects[1].transform.localPosition = Initial_Transforms_MainMenu[1];//Gamelogo
-        Main_Menu_Objects[3].transform.localPosition = Initial_Transforms_MainMenu[2];
-        Main_Menu_Objects[4].transform.localPosition = Initial_Transforms_MainMenu[3];
-        Main_Menu_Objects[5].transform.localPosition = Initial_Transforms_MainMenu[4];
-        Main_Menu_Objects[6].transform.localPosition = Initial_Transforms_MainMenu[5];
-        Main_Menu_Objects[7].transform.localPosition = Initial_Transforms_MainMenu[6];
+        Main_Menu_Objects[1].transform.localPosition = Initial_Transforms_MainMenu[0];//Gamelogo
+        Main_Menu_Objects[3].transform.localPosition = Initial_Transforms_MainMenu[1];
+        Main_Menu_Objects[4].transform.localPosition = Initial_Transforms_MainMenu[2];
+        Main_Menu_Objects[5].transform.localPosition = Initial_Transforms_MainMenu[3];
+        Main_Menu_Objects[6].transform.localPosition = Initial_Transforms_MainMenu[4];
+        Main_Menu_Objects[7].transform.localPosition = Initial_Transforms_MainMenu[5];
 
 
         Main_Menu_Objects[0].transform.localScale = Initial_Scale_MainMenu[0];//rectangle
@@ -70,7 +73,7 @@ public class PanlesManager : MonoBehaviour
 
        Main_Menu_Objects[1].transform.localRotation = Quaternion.Euler(Initial_Transforms_MainMenu[0]);//Gamelogo                                       
 
-
+        
 
     }
 
@@ -83,16 +86,7 @@ public class PanlesManager : MonoBehaviour
 
     void Start()
     {
-        Main_Menu_Objects[0].transform.DOScale(Main_Menu_New_Scals[0], Duration);
-        Main_Menu_Objects[1].transform.DOScale(Main_Menu_New_Scals[1], Duration2);
-        Main_Menu_Objects[1].transform.DOMoveX(Main_Menu_New_Pos[4], Duration2).SetEase(Ease.OutExpo);
-        Main_Menu_Objects[1].transform.DORotate(circle, Duration2, RotateMode.FastBeyond360);
-        Main_Menu_Objects[2].transform.DOScale(Main_Menu_New_Scals[2], Duration);
-        Main_Menu_Objects[3].transform.DOMoveX(Main_Menu_New_Pos[0], Duration2);
-        Main_Menu_Objects[4].transform.DOMoveY(Main_Menu_New_Pos[1], Duration2 * 1 / 4).SetEase(Ease.OutBack);
-        Main_Menu_Objects[5].transform.DOMoveY(Main_Menu_New_Pos[2], Duration2 * 1 / 3).SetEase(Ease.OutBack);
-        Main_Menu_Objects[6].transform.DOMoveY(Main_Menu_New_Pos[2], Duration2 * 1 / 2).SetEase(Ease.OutBack);
-        Main_Menu_Objects[7].transform.DOMoveY(Main_Menu_New_Pos[3], Duration2 * 1).SetEase(Ease.OutBack);
+        Main_Menu_Tweens();
     }
 
    
