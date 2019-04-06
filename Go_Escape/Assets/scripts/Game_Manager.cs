dbeb45adf86e6c635bf7134bcae7c2f960bcf0fa
jamilof <46740397+jamilof@users.noreply.github.com>
2019-04-06 10:59:12 +0300

    انا ما عملت شي والله

--- Go_Escape/Assets/scripts/Game_Manager.cs
+++ Go_Escape/Assets/scripts/Game_Manager.cs
@@ -8,7 +8,8 @@ public class Game_Manager : MonoBehaviour
     
     void Start()
     {
-        
+        Dictionary<int, string> levels = new Dictionary<int, string>();
+        levels.Add(1, "level1");
     }
     
 }
